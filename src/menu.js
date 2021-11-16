8dc2d8e0314b8d9887bebc7ec3883105dc8c4085
Gyromitte <gyromitte@gmail.com>
2021-11-15 19:12:25 -0600

    Converted menu tab html code into javaScript dynamic  code

--- src/menu.js
+++ src/menu.js
@@ -27,3 +27,30 @@ Menu.id = "selected";
 
 createButtons("Contact");
 
+const menuInfo = document.createElement('div');
+menuInfo.id = "Menu-info";
+content.appendChild(menuInfo);
+
+const tabTitle = document.createElement('p');
+tabTitle.classList = "tab-title";
+tabTitle.innerText = "Menu";
+menuInfo.appendChild(tabTitle);
+
+const menuContent = document.createElement('menu');
+menuInfo.appendChild(menuContent);
+
+function createMenuImages(nameItem, source){
+    let newImage = document.createElement('div');
+    newImage.classList = "menu-images-container";
+    menuContent.appendChild(newImage);
+    let image = document.createElement('img');
+    image.classList = "menu-images";
+    image.src = source;
+    newImage.innerText = nameItem;
+    newImage.appendChild(image);
+}
+
+createMenuImages("Individual Nigiri's", "images/menu/salmon-nigiri.jpg");
+createMenuImages("California Roll's", "images/menu/california-roll.jpg");
+createMenuImages("Makizushi", "images/menu/makizushi.jpg");
+
