06f5abb135f38091a1cd9f40b9c51ec8f154c57c
Gyromitte <gyromitte@gmail.com>
2021-11-22 20:40:16 -0600

    Added clearContent function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 showHomeTab();
 
+function clearContent(){
+    document.getElementById('content').innerHTML = "";
+}
+
 const Home = document.getElementById('selected');
 Home.addEventListener('click', e=>{
+    clearContent();
     showHomeTab();
 });
 
 
 Menu.addEventListener('click', e=>{
+    clearContent();
     showMenuTab();
 });
 
 
 Contact.addEventListener('click', e=>{
+    clearContent();
     showContactTab();
 });
